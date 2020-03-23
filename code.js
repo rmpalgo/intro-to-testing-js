1cceb9112bb3cd4bf0de7f0e0f6b6a580480c9c0
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-03-23 14:11:18 -0500

    Completed first fail test for sayHello function.

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
-// helloWorld function 
-function helloWorld() {
+// helloWorld function
+//BOTH statement and anonymouse expression runs because functions are first class in JS
+var helloWorld = function()  {
     return "Hello, World!";
 }
+
